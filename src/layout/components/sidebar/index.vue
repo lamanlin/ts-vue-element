1d1864582970995d26d37ef0478c53eb2ce96d94
lindongjian <Laman_lin@126.com>
2020-06-14 16:13:10 +0800

    business

--- src/layout/components/sidebar/index.vue
+++ src/layout/components/sidebar/index.vue
@@ -1,41 +1,38 @@
 <template>
   <div class="side-bar">
     <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu :show-timeout="200"
-               :default-active="$route.path"
-               mode="vertical"
-               :background-color="variables.menuBg"
-               :text-color="variables.menuText">
-        <sidebar-item v-for="route in routes"
-                      :key="route.name"
-                      :item="route"
-                      :base-path="route.path" />
+      <el-menu
+        :show-timeout="200"
+        :default-active="$route.path"
+        mode="vertical"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+      >
+        <sidebar-item v-for="route in routes" :key="route.name" :item="route" :base-path="route.path" />
       </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator'
-  import SidebarItem from './SidebarItem.vue'
-  import variables from '@/styles/variables.less'
-  @Component({
-    name: 'Sidebar',
-    components: {
-      SidebarItem
-    }
-  })
-  export default class ClassName extends Vue {
-    private get routes(): [] {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const router: any = this.$router
-      return router.options.routes
-    }
-    private  get variables() {
+import { Component, Vue } from 'vue-property-decorator'
+import SidebarItem from './SidebarItem.vue'
+import variables from '@/styles/variables.less'
+@Component({
+  name: 'Sidebar',
+  components: {
+    SidebarItem
+  }
+})
+export default class ClassName extends Vue {
+  private get routes(): [] {
+    const router: any = this.$router
+    return router.options.routes
+  }
+  private get variables() {
     return variables
   }
-  }
+}
 </script>
 
-<style lang="less" scope>
-</style>
+<style lang="less" scope></style>
--- src/views/business/test.vue
+++ src/views/business/test.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
-    24234vb
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">查询</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
-  @Component
-export default class ClassName extends Vue {}
+@Component
+export default class ClassName extends Vue {
+  private formInline = {
+    user: '',
+    region: ''
+  }
+}
 </script>
 
-<style lang="less" scope>
-</style>
+<style lang="less" scope></style>
